d3b73ecc6fe13b1e47f65c329e619a8b0bf7330e
juanrabaro <jrbarolop@gmail.com>
2024-05-29 14:45:20 +0200

    empezando hacer menu hamburguesa

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -5,16 +5,25 @@
   import { user, loginStore, logoutStore } from "$lib/stores/authStore.js";
   import { verifyToken } from "$lib/api/auth.js";
   import { initLoading, endLoading } from "$lib/stores/loadingLogoutStore.js";
-  
+  import Menu from "$lib/assets/menu.png";
+
   onMount(async () => {
     try {
       const token = Cookies.get("token");
-      const res = await verifyToken({token});
+      const res = await verifyToken({ token });
       loginStore(res.data);
     } catch (err) {
-      //console.error(err);
       console.log("Error con el token o no encontrado en el header");
     }
+
+    const links = document.querySelectorAll('nav ul li a');
+    const checkbox = document.querySelector('.checkbox-menu');
+
+    links.forEach(link => {
+      link.addEventListener('click', () => {
+        checkbox.checked = false;
+      });
+    });
   });
 
   let userHeader;
@@ -34,18 +43,24 @@
 <header>
   <h1>Task Master</h1>
   <nav>
-    <ul>
-      {#if userHeader}
+    {#if userHeader}
+    <label for="menu-hamburger" class="hamburger-menu-icon">
+      <img src={Menu} alt="menu">
+    </label>
+    <input id="menu-hamburger" class="checkbox-menu" type="checkbox" />
+      <ul>
         <li><a href="/">Tasks</a></li>
         <li><a href="/time-tasks">TimeTasks</a></li>
         <li><a href="/stadistics">Stadistics</a></li>
         <li><a href="/profile">Profile</a></li>
         <li><button on:click={logout}>Logout</button></li>
-      {:else}
+      </ul>
+    {:else}
+      <ul>
         <li><a href="/login">Login</a></li>
         <li><a href="/register">Register</a></li>
-      {/if}
-    </ul>
+      </ul>
+    {/if}
   </nav>
 </header>
 
@@ -64,6 +79,14 @@
     }
 
     nav {
+      .checkbox-menu:checked ~ ul {
+        display: flex;
+      }
+
+      .checkbox-menu {
+        display: none;
+      }
+
       ul {
         display: flex;
         flex-direction: row;
@@ -95,6 +118,37 @@
             cursor: pointer;
             background-color: #740808;
             border: 1px solid #df7171;
+          }
+        }
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    header {
+      nav {
+        .checkbox-menu {
+          display: block;
+        }
+        .checkbox-menu:checked ~ ul {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+        }
+        ul {
+          display: none;
+
+          li {
+            padding: 10px 0 10px 0;
+
+            a {
+              font-size: 1.5em;
+            }
+
+            button {
+              font-size: 1.5em;
+            }
           }
         }
       }

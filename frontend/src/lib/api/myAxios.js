1ad6f77b40579d153592e07065b7d8a34c4917c5
juanrabaro <jrbarolop@gmail.com>
2024-06-04 14:43:10 +0200

    tocando axios

--- frontend/src/lib/api/myAxios.js
+++ frontend/src/lib/api/myAxios.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 let myAxios;
 
-if (import.meta.env.MODE === "desarrollo") {
-  myAxios = axios.create({
-    baseURL: import.meta.env.LOCAL_API_URL,
-    withCredentials: true,
-  })
-} else if (import.meta.env.MODE === "produccion") {
+if (import.meta.env.MODE === "produccion") {
   myAxios = axios.create({
     baseURL: import.meta.env.DESPLIEGUE_API_URL,
+    withCredentials: true,
+  })
+} else {
+  myAxios = axios.create({
+    baseURL: import.meta.env.LOCAL_API_URL,
     withCredentials: true,
   })
 }
